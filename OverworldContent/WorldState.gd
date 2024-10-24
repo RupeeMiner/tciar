22e3784836ddb666ba72175cf88d2617b5de9c7d
Neaves, Maeby <mneaves@iu.edu>
2024-10-24 09:46:31 -0400

    imported dialogic

--- OverworldContent/WorldState.gd
+++ OverworldContent/WorldState.gd
@@ -3,6 +3,7 @@ extends Node
 signal battle_started(current_enemy)
 signal battle_ended()
 
+var current_ingredients = ["Beef Patty", "Hamburger Bun", "Cheese Slice"]
 var current_enemies = ["res://Resources/Beefy.tres", "res://Resources/Bun.tres", "res://Resources/Cheese.tres"]
 
 func start_battle():
