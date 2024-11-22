d04ca9b14c3759454d142212741aa37c38a266a9
Neaves, Maeby <mneaves@iu.edu>
2024-11-22 00:06:58 -0500

    start of enemy avoiding walls
    broken and bad but it is 12am

--- OverworldContent/Enemy/overworld_enemy.gd
+++ OverworldContent/Enemy/overworld_enemy.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+var sight_range = 100
 var speed = 80
 var directions = [Vector2(0,-1), Vector2(0, 1), Vector2(-1, 0), Vector2(1,0)]
 var player
@@ -8,16 +9,28 @@ func _ready():
 	player = get_tree().get_first_node_in_group("Player")
 
 func _physics_process(delta):
-	var player_dir = position - player.position
-	var player_dot_prods = []
-	for dir in directions:
-		player_dot_prods.append(dir.dot(player_dir.normalized()))
+	var contexts = [0,0,0,0]
 	
-	var contexts = player_dot_prods
-	var index = contexts.find(contexts.max())
+	var player_dir = global_position - player.global_position
+	if (player_dir.length() < 60):
+		for i in range(directions.size()):
+			contexts[i] += directions[i].dot(player_dir.normalized())
 	
-	print(directions[index])
-	velocity.x = directions[index][0] * speed
-	velocity.y = directions[index][1] * speed
+	var i = 0
+	for raycast in $ContextMap/Orthogonal.get_children():
+		if (raycast.is_colliding()):
+			contexts[i] -= 5
+		i +=1
+	
+	var index
+	if (contexts.count(contexts.max() == contexts.size() - 1)):
+		if (contexts.find(contexts.min()) in [0, 3]):
+			index = contexts.find(contexts.min()) + 1
+		else:
+			index = contexts.find(contexts.min()) - 1
+	elif (contexts.count(contexts.max() == contexts.size())):
+		index = contexts.find(contexts.max())
+	velocity.x = directions[index][0] * speed - (velocity.x * .9)
+	velocity.y = directions[index][1] * speed - (velocity.y * .9)
 	
 	move_and_slide()
