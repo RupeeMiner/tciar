35ac0d194ef86a90d18f74bc35e39af110e28f48
Neaves, Maeby <mneaves@iu.edu>
2024-12-11 23:31:08 -0500

    started implementing textured battle scene

--- BattleSceneContent/battle_scene.gd
+++ BattleSceneContent/battle_scene.gd
@@ -19,41 +19,46 @@ func init(current_enemy, enemy_health):
 	
 	visible = true
 	
-	$Textbox.hide()
-	$Actions.hide()
+	$AspectRatioContainer/BattleBox.hide()
+	$AspectRatioContainer/FlavorText.hide()
+	$AspectRatioContainer/HpBox.hide()
+	$Moves.hide()
 	
-	$EnemyTexture.texture = enemy.texture
+	$EnemySprite.texture = enemy.texture
 	
-	$Actions/MovesMenu/Move1.text = PlayerState.moves[0]
-	$Actions/MovesMenu/Move2.text = PlayerState.moves[1]
-	$Actions/MovesMenu/Move3.text = PlayerState.moves[2]
+	$Moves/Move1.texture_normal = load("res://Resources/battleUI/buttons/" + PlayerState.moves[0] + ".png")
+	$Moves/Move2.texture_normal = load("res://Resources/battleUI/buttons/" + PlayerState.moves[1] + ".png")
+	$Moves/Move3.texture_normal = load("res://Resources/battleUI/buttons/" + PlayerState.moves[2] + ".png")
 	
-	set_health(PlayerState.current_health, PlayerState.max_health, $PlayerHealth)
-	set_health(enemy_health, enemy.health, $EnemyHealth)
+	set_health(PlayerState.current_health, PlayerState.max_health, $AspectRatioContainer/HpBox/Label)
 	
 	current_enemy_health = enemy_health
 	current_player_health = PlayerState.current_health
 	
 	AudioManager.update_music("Battle")
 	SceneTransition.fade_in()
+	$AspectRatioContainer/BattleTextBox.show()
 	display_text("%s appeared!" % enemy.name)
 	await textbox_closed
 	
+	$AspectRatioContainer/HpBox.show()
 	if (enemy.speed > PlayerState.speed):
 		enemy_turn()
 	else:
-		$Actions.show()
-		$Actions/MovesMenu.hide()
+		$Moves.show()
+		$BattleTextBox.hide()
 
 func _input(event):
-	if (Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)) and $Textbox.is_visible_in_tree():
-		$Textbox.hide()
+	if (Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)) and $AspectRatioContainer/BattleTextBox.visible:
+		$AspectRatioContainer/BattleTextBox.hide()
 		emit_signal("textbox_closed")
 
 func display_text(text):
-	$Actions.hide()
-	$Textbox/Label.text = text
-	$Textbox.show()
+	$Moves.hide()
+	$AspectRatioContainer/BattleBox.hide()
+	$AspectRatioContainer/FlavorText.hide()
+	$AspectRatioContainer/BattleTextBox/Label.text = text
+	$AspectRatioContainer/BattleTextBox.show()
 
 func set_health(health, max_health, health_label):
 	health_label.text = "HP: %d/%d" % [health, max_health]
@@ -66,7 +71,7 @@ func enemy_turn():
 	var damage = enemy.attack - PlayerState.defense
 	
 	current_player_health = max(0, current_player_health - damage)
-	set_health(current_player_health, PlayerState.max_health, $PlayerHealth)
+	set_health(current_player_health, PlayerState.max_health, $AspectRatioContainer/HpBox/Label)
 	
 	display_text("%s dealt %d damage" % [enemy.name, damage])
 	await textbox_closed
@@ -80,9 +85,7 @@ func enemy_turn():
 		get_tree().reload_current_scene()
 		visible = false
 	
-	$Actions.show()
-	$Actions/ActionMenu.show()
-	$Actions/MovesMenu.hide()
+	$Moves.show()
 
 func player_attack(move_num):
 	display_text("You used %s!" % PlayerState.moves[move_num])
@@ -95,7 +98,6 @@ func player_attack(move_num):
 		damage = damage + 2
 	
 	current_enemy_health = max(0, current_enemy_health - damage)
-	set_health(current_enemy_health, enemy.health, $EnemyHealth)
 	
 	display_text("You dealt %d damage" % damage)
 	await textbox_closed
@@ -123,14 +125,6 @@ func end_battle():
 	visible = false
 	AudioManager.update_music("Dungeon")
 	SceneTransition.fade_in()
-
-func _on_moves_pressed() -> void:
-	$Actions/ActionMenu.hide()
-	$Actions/MovesMenu.show()
-
-func _on_move_back_pressed() -> void:
-	$Actions/MovesMenu.hide()
-	$Actions/ActionMenu.show()
 
 func _on_move_1_pressed() -> void:
 	player_attack(0)
